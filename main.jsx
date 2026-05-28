b1ca2ea0e2ca2ddfda314b8aec9fde1ca5ae135f
grantpack2012-ui <grant.pack.2012@gmail.com>
2026-05-27 22:00:30 -0700

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import DragonRoseApp from './dragon-rose';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <DragonRoseApp />
+  </React.StrictMode>
+);
